102b9822437a05a007f7197299d1adbbc4998528
Christopher Briggs <confusedmatrix@gmail.com>
2022-04-19 08:44:18 +0100

    Improvements to the opendatasoft harvester

--- src/ckanext-subakdc-plugins/ckanext/schema/plugin.py
+++ src/ckanext-subakdc-plugins/ckanext/schema/plugin.py
@@ -6,22 +6,21 @@ import ckan.plugins as p
 
 log = logging.getLogger(__name__)
 
-def get_taxonomy_dict(field):
+def get_taxonomy_dict(field=None):
     with open(path.join(path.dirname(__file__), "taxonomy.json")) as f:
         taxonomy_cats = json.load(f)
 
     return list(map(lambda tc: {"value": tc, "label": tc}, taxonomy_cats))
 
-def get_countries_dict(field):
-    with open(path.join(path.dirname(__file__), "countries-iso-3166.json")) as f:
+def get_countries_dict(field=None):
+    with open(path.join(path.dirname(__file__), "..", "countries-iso-3166.json")) as f:
         countries = json.load(f)
 
     return list(map(lambda c: {"value": c["alpha-2"], "label": c["name"]}, countries))
 
 def get_country_name_from_code(code):
-    with open(path.join(path.dirname(__file__), "countries-iso-3166.json")) as f:
-        countries = json.load(f)
-    codes_to_names = {c["alpha-2"]: c["name"] for c in countries}
+    countries = get_countries_dict()
+    codes_to_names = {c["value"]: c["label"] for c in countries}
     if code == "[]" or code is None:
         return None
     return codes_to_names[code]
