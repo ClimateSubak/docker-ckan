bac8c2a45ecb5d0eb4af841eecb2830e40d4ecd6
Rabscuttler <laurence.watson@gmail.com>
2022-04-22 16:35:04 +0100

    adds harvester for amazon sustainability data registry

--- src/ckanext-subakdc-plugins/ckanext/harvesters/__init__.py
+++ src/ckanext-subakdc-plugins/ckanext/harvesters/__init__.py
@@ -1,3 +1,4 @@
 from ckanext.harvesters.opendatasoft_harvester import OpendatasoftHarvester
+from ckanext.harvesters.asdi_harvester import ASDIHarvester
 
-__all__ = [ 'OpendatasoftHarvester' ]
+__all__ = [ 'OpendatasoftHarvester', 'ASDIHarvester' ]
