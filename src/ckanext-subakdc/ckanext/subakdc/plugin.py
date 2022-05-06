be8157b66b6dd7a6de19eac9b29d41d2df3d7316
Christopher Briggs <confusedmatrix@gmail.com>
2022-05-06 10:38:19 +0100

    Set pre-defined quick explore tags on homepage

--- src/ckanext-subakdc/ckanext/subakdc/plugin.py
+++ src/ckanext-subakdc/ckanext/subakdc/plugin.py
@@ -1,12 +1,37 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+
+HOMEPAGE_TAGS = [
+    "CO2",
+    "Energy",
+    "Agriculture",
+    "Wind",
+    "Solar",
+    "Electricity",
+    "Crude Oil",
+]
 
 
-class SubakdcPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+def homepage_tags():
+    tags = tk.g.search_facets["tags"]["items"]
+    filtered_tags = list(filter(lambda tag: tag["name"] in HOMEPAGE_TAGS, tags))
+
+    return filtered_tags
+
+
+class SubakdcPlugin(p.SingletonPlugin):
+    p.implements(p.IConfigurer)
+    p.implements(p.ITemplateHelpers)
 
     # ------- IConfigurer method implementations ------- #
     def update_config(self, config):
-        toolkit.add_template_directory(config, 'templates')
-        toolkit.add_public_directory(config, 'public')
-        toolkit.add_resource('./assets', 'ckanext-subakdc')
+        tk.add_template_directory(config, "templates")
+        tk.add_public_directory(config, "public")
+        tk.add_resource("./assets", "ckanext-subakdc")
+
+    # ------- ITemplateHelpers method implementations ------- #
+    def get_helpers(self):
+        """
+        Helper function to define homepage quick explore tags
+        """
+        return {"homepage_quick_explore_tags": homepage_tags}
