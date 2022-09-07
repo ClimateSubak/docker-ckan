5a92e3af74f28b9359a25540ace07cc3c6ccfc4b
Christopher Briggs <confusedmatrix@gmail.com>
2022-09-07 15:39:52 +0100

    Started zenodo harvester work

--- src/ckanext-subakdc-plugins/ckanext/harvesters/__init__.py
+++ src/ckanext-subakdc-plugins/ckanext/harvesters/__init__.py
@@ -1,4 +1,5 @@
 from ckanext.harvesters.opendatasoft_harvester import OpendatasoftHarvester
 from ckanext.harvesters.asdi_harvester import ASDIHarvester
+from ckanext.harvesters.zenodo_harvester import ZenodoHarvester
 
-__all__ = [ 'OpendatasoftHarvester', 'ASDIHarvester' ]
+__all__ = ["OpendatasoftHarvester", "ASDIHarvester", "ZenodoHarvester"]
