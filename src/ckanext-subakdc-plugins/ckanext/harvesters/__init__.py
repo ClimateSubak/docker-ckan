be503822e0c53d7d325b412e3dab481c12391ba7
Chris Briggs <confusedmatrix@gmail.com>
2022-12-13 15:47:14 +0000

    Added harvester for data.europa.eu datasets

--- src/ckanext-subakdc-plugins/ckanext/harvesters/__init__.py
+++ src/ckanext-subakdc-plugins/ckanext/harvesters/__init__.py
@@ -2,5 +2,6 @@ from ckanext.harvesters.opendatasoft_harvester import OpendatasoftHarvester
 from ckanext.harvesters.asdi_harvester import ASDIHarvester
 from ckanext.harvesters.zenodo_harvester import ZenodoHarvester
 from ckanext.harvesters.figshare_harvester import FigshareHarvester
+from ckanext.harvesters.europa_harvester import EuropaHarvester
 
-__all__ = ["OpendatasoftHarvester", "ASDIHarvester", "ZenodoHarvester", "FigshareHarvester"]
+__all__ = ["OpendatasoftHarvester", "ASDIHarvester", "ZenodoHarvester", "FigshareHarvester", "EuropaHarvester"]
