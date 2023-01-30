560fb6e366d9477063cb1c85ae928d1d1a801564
Chris Briggs <confusedmatrix@gmail.com>
2023-01-30 15:53:44 +0000

    removed log statements form external links js file

--- src/ckanext-subakdc/ckanext/subakdc/assets/js/external-links.js
+++ src/ckanext-subakdc/ckanext/subakdc/assets/js/external-links.js
@@ -1,14 +1,11 @@
 $(function () {
     // Force all external links to open in new tab
     var links = document.querySelectorAll('a')
-    console.log(links)
     for (var i = 0; i < links.length; i++) {
         var link = links[i]
         loc = link.getAttribute("href")
-        console.log(loc)
         if (loc != null && loc.startsWith("http") && !loc.startsWith("http://data.subak.org")) {
             link.setAttribute("target", "_blank")
-            console.log('^ external')
         }
     }
 })
